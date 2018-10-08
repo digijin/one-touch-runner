c121cbf9e0388c12ef87f7d011037acc11c74811
James Smith <James.Smith@foxsports.com.au>
2018-10-08 05:59:16 +0100

    few obstacles

--- src/Enemy.js
+++ src/Enemy.js
@@ -1,14 +1,28 @@
 import * as PIXI from 'pixi.js';
 
+import wickets from './assets/wickets.png';
+import esky from './assets/esky.png';
+import ball from './assets/ball.png';
+import chips from './assets/HotChips.png';
+
+const images = [wickets,
+  esky,
+  ball,
+  chips,
+];
+
 
 export default class Enemy extends PIXI.Sprite {
   constructor() {
-    super(PIXI.Texture.WHITE);
-    this.tint = 0xff0000;
+    const img = images[Math.floor(images.length * Math.random())];
+    const texture = new PIXI.Texture(new PIXI.BaseTexture(img));
+
+    super(texture);
+    // this.tint = 0xff0000;
     this.x = window.innerWidth;
     this.y = 200;
-    this.width = Math.random() * 100;
-    this.height = Math.random() * 100;
+    this.width = 100;
+    this.height = 100;
   }
 
   update(delta) {
