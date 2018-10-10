fad4226fd2205dc71b5d8c6fae625c04a673ed02
James Smith <James.Smith@foxsports.com.au>
2018-10-10 07:03:42 +0100

    starting state

--- src/Background.js
+++ src/Background.js
@@ -3,14 +3,19 @@ import config from './config';
 
 import img from './assets/CricketPitch.jpg';
 
+import { PlayerState } from './Game';
+
 export default class Background extends PIXI.extras.TilingSprite {
-  constructor() {
+  constructor(app) {
     const texture = new PIXI.Texture(new PIXI.BaseTexture(img));
     super(texture, config.stage.width, config.stage.height);
+    this.app = app;
     // this.tint = 0xff0000;
   }
 
   update(delta) {
-    this.tilePosition.x -= delta * config.scrollSpeed;
+    if (this.app.state === PlayerState.RUN || this.app.state === PlayerState.JUMP) {
+      this.tilePosition.x -= delta * config.scrollSpeed;
+    }
   }
 }
--- src/Objects.js
+++ src/Objects.js
@@ -1,15 +1,23 @@
 import * as PIXI from 'pixi.js';
 
 import Enemy from './Enemy';
+import { PlayerState } from './Game';
 
 export default class Objects extends PIXI.Container {
     counter = 100;
 
+    constructor(app) {
+      super();
+      this.app = app;
+    }
+
     update(delta) {
-      this.counter -= delta;
-      if (this.counter < 0) {
-        this.counter = 10 + Math.random() * 80;
-        this.addChild(new Enemy());
+      if (this.app.state === PlayerState.RUN || this.app.state === PlayerState.JUMP) {
+        this.counter -= delta;
+        if (this.counter < 0) {
+          this.counter = 10 + Math.random() * 80;
+          this.addChild(new Enemy());
+        }
       }
     }
 }
