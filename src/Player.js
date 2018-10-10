221f3a49f7e09296884d4681ed587de6ce335d57
James Smith <James.Smith@foxsports.com.au>
2018-10-10 03:30:44 +0100

    animations

--- src/Player.js
+++ src/Player.js
@@ -2,33 +2,68 @@ import * as PIXI from 'pixi.js';
 
 import config from './config';
 
-import img from './assets/player.png';
+// import standing from './assets/player.png';
 
-export default class Player extends PIXI.Sprite {
+import walk1 from './assets/Shane_Po1.png';
+import walk2 from './assets/Shane_Po2.png';
+import walk3 from './assets/Shane_Po3.png';
+
+import jump from './assets/ShaneJump.png';
+
+const States = {
+  WAIT: 'wait',
+  RUN: 'run',
+  JUMP: 'jump',
+};
+
+
+const walktextures = [walk1, walk2, walk3]
+  .map(i => new PIXI.Texture(new PIXI.BaseTexture(i)));
+const jumptextures = [jump]
+  .map(i => new PIXI.Texture(new PIXI.BaseTexture(i)));
+
+
+export default class Player extends PIXI.extras.AnimatedSprite {
     h=0;
 
+    state = States.WAIT;
+
     constructor() {
-      const baseTexture = new PIXI.BaseTexture(img);
-      const texture = new PIXI.Texture(baseTexture);
-      super(texture);
+      // const baseTexture = new PIXI.BaseTexture(standing);
+      // const texture = new PIXI.Texture(baseTexture);
+      super(walktextures, false);
       this.width = 100;
       this.height = 200;
       //   this.tint = 0;
       this.anchor = { x: 0.5, y: 1 };
       this.x = 100;
+      this.y = config.ground;
 
       document.addEventListener('mousedown', () => {
-        this.h = -20;
+        if (this.state !== States.JUMP) {
+          this.state = States.JUMP;
+          this.h = -20;
+          this.textures = jumptextures;
+        }
+        // this.textures = walktextures;
       });
+      // this.gotoAndPlay(1)
+      this.animationSpeed = 0.5;
+      this.play();
     }
 
     update(delta) {
-      this.h += delta;
-
-      this.y += this.h;
-      if (this.y > config.ground) {
-        this.y = config.ground;
-        this.h = 0;
+      super.update(delta);
+      if (this.state === States.JUMP) {
+        this.h += delta;
+        this.y += this.h;
+        if (this.y > config.ground) {
+          this.state = States.RUN;
+          this.y = config.ground;
+          this.textures = walktextures;
+          // this.gotoAndPlay(1);
+          this.h = 0;
+        }
       }
     }
 }
